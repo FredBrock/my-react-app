9104211c0f72bcd8e3ca1f20ef6beca636f98b98
fredbrock <fredbrock2018@outlook.com>
2023-11-29 14:36:44 +0800

    feat: 提交

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+// import "./App.css";
+import { useState, useRef, useEffect } from "react";
 
+function VideoPlayer({ src, isPlaying }) {
+  const ref = useRef(null);
+  useEffect(() => {
+    console.log(ref.current);
+    if (isPlaying) {
+      ref.current.play(); // 渲染期间不能调用 `play()`。
+    } else {
+      ref.current.pause(); // 同样，调用 `pause()` 也不行。
+    }
+  }, []);
+
+  return <video ref={ref} src={src} loop playsInline />;
+}
 function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [text, setText] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +34,17 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <>
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+        <button onClick={() => setIsPlaying(!isPlaying)}>
+          {isPlaying ? "暂停" : "播放"}
+        </button>
+        <VideoPlayer
+          isPlaying={isPlaying}
+          src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+        />
+      </>
     </div>
   );
 }
